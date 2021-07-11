00fb658269f22711169cc08014bf17e4899bf9f1
HillLuke <lukehilldev@gmail.com>
2021-07-11 17:12:25 +0100

    Add FPS counter to display dps, set vsync and target framerate

---
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int AverageFrameRate;
+    public TextMeshProUGUI Text;
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 60;
+    }
+
+
+    private void Update()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        AverageFrameRate = (int)current;
+        Text.text = AverageFrameRate.ToString() + " FPS";
+    }
+}
